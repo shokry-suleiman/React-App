4eff208a038e9041dd30b76ae8358f9a690f8adc
Shoukry <shokrysuleiman032@gmail.com>
2022-09-23 21:05:25 +0200

    feat(feat): :sparkles: Create React Components

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+function Todo(props)  {
+    function deleteHandler(){
+        console.log('clicked!')
+        console.log(props.title)
+    }
+    return (
+        <div className="card">
+    <h2>{props.title}</h2>
+    <div className="actions">
+    <button className="btn" onClick={deleteHandler}>Delete </button>
+      </div>
+  </div>
+    )
+}
+
+export default Todo;
